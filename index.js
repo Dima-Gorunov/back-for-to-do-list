b9462817d07be858489ccd019b151245475103e0
Dima-Gorunov <gorunov-01@mail.ru>
2022-11-21 10:33:36 +0300

    up

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('/api', (req, res) => {
-    res.json("qweqwew")
+app.use('/', (req, res) => {
+    res.send("weterjgklfjgdfkljg")
 })
+//qweqw
 const start = async () => {
     try {
         await sequelize.authenticate()
