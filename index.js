49990ea685c81da905a94a2fb13d4b9cce14cd29
Dima-Gorunov <gorunov-01@mail.ru>
2022-11-21 10:37:22 +0300

    up

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("weterjgklfjgdfkljg")
 })
 //qweqw
